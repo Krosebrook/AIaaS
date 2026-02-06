24202f30d16781fc3643e4075f54fe768dead3d6
Kyle <kylerosebrook@gmail.com>
2026-02-06 17:04:58 +0000

    File changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { createPageUrl } from './utils';
+import { createPageUrl } from '../utils';
 import { Shield, Target, Users, Award } from 'lucide-react';
 
 export default function About() {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { createPageUrl } from './utils';
+import { createPageUrl } from '../utils';
 import { 
   ChevronDown, 
   Sparkles, 
